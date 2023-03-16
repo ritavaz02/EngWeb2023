8ebcb68181c41b77e86d611ca1cd920f216da56a
ritavaz02 <anaritavaz02@gmail.com>
2023-03-16 23:43:19 +0000

    tpc4 updates

--- TPC4/static.js
+++ TPC4/static.js
@@ -8,7 +8,14 @@
 var fs = require('fs')
 
 function staticResource(request){
-    return /\/w3.css$/.test(request.url)
+    var files = fs.readdirSync('./public/');
+    var bool = false
+    files.forEach(file => {
+        let pattern = new RegExp(file)
+        
+        if(pattern.test(request.url)) bool = true
+    })
+    return bool
 }
 
 exports.staticResource = staticResource
@@ -16,17 +23,26 @@ exports.staticResource = staticResource
 function serveStaticResource(req, res){
     var partes = req.url.split('/')
     var file = partes[partes.length -1 ]
-    fs.readFile('public/' + file, (error, data)=>{
-        if(error){
-            console.log('Error: File not found ' + error)
+    fs.readFile('./public/' + file, (erro, dados)=>{
+        if(erro){
+            console.log('Erro: ficheiro não encontrado ' + erro)
             res.statusCode = 404
-            res.end('Error: File not found ' + error)
+            res.end('Erro: ficheiro não encontrado ' + erro)
         }
         else{
-            if(file == 'w3.css'){
-                res.setHeader('Content-Type', 'text/css')
-                res.end(data)
+            if(file == 'favicon.ico'){
+                res.setHeader('Content-Type', 'image/x-icon')
+                res.end(dados)
             }
+            else if(file == 'w3.css'){
+                res.setHeader('Content-Type', 'text/css')
+                res.end(dados)
+            }
+            // PNG images
+            else{
+                res.setHeader('Content-Type', 'image/png')
+                res.end(dados)
+            }    
         }
     })
 }
